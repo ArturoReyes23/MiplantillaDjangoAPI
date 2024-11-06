54e4795516eccdf5cdf19efc3d5a1656b254711d
ArturoReyes23 <2022451030@teschi.edu.mx>
2024-11-05 22:36:27 -0600

    Mi platilla

--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def login_views(request):
     template_name="authentication-login.html"
-    
+    return render(request,template_name)
+
+# View for register
+def register_view(request):
+    template_name="authentication-register.html"
+    return render(request,template_name)
+
+# View for forgot
+def forgot_view(request):
+    template_name="authentication-password.html"
     return render(request,template_name)
